c17f32070cce279a1ed33a53d049b816d958ac76
Kavan Mevada <kavanmevada@gmail.com>
2019-05-31 23:28:53 -0400

    Add files via upload

---
+++ makeKernel.sh
@@ -0,0 +1,32 @@
+# Reset repos [optional]
+# rm /etc/apt/sources.list
+
+# Add repos [optional]
+# echo "deb http://deb.debian.org/debian buster main" >> /etc/apt/sources.list
+# echo "deb http://deb.debian.org/debian buster-updates main" >> /etc/apt/sources.list
+# echo "deb http://security.debian.org buster/updates main" >> /etc/apt/sources.list
+
+
+checkCommandExist(){
+ if ! [ -x "$(command -v $1)" ]; then
+  sudo apt-get -y install $1
+  else echo 'LOG: '$1' is installed.'
+ fi
+}
+
+checkCommandExist build-essential
+checkCommandExist linux-source
+checkCommandExist bc
+checkCommandExist kmod
+checkCommandExist cpio
+checkCommandExist flex
+checkCommandExist libncurses5-dev
+
+checkCommandExist libelf-dev
+checkCommandExist libssl-dev:native
+
+# tar xf '/media/kavan/OTHER/Linux/linux-5.1.6.tar.xz' -C kavan/
+
+# make nconfig
+
+# make -j`nproc` bindeb-pkg
